ed0809d1ceb73e122224195741232630c7f6b056
Cem Kaya <cem.kaya@zohomail.com>
2025-05-05 22:51:48 +0300

    feat: add PairsDemo with generic implementation
    Creates a new project demonstrating generic type parameters
    with a simple Pair<T> class. Includes null checking in constructor
    and reset methods showcasing proper handling of generic values.

---
+++ src/Generics/PairsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿var pairsOfInt = new Pair<int>(1, 2);
+Console.WriteLine($"First: {pairsOfInt.First}, Second: {pairsOfInt.Second}");
+pairsOfInt.ResetFirst(); // will set First to default (0)
+Console.WriteLine($"First: {pairsOfInt.First}, Second: {pairsOfInt.Second}");
+
+internal class Pair<T>
+{
+  public T? First { get; private set; }
+  public T? Second { get; private set; }
+
+  public Pair(T? first, T? second)
+  {
+    First = first;
+    Second = second;
+  }
+
+  public void ResetFirst() => First = default;
+
+  public void ResetSecond() => Second = default;
+}
